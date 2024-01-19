3a8b1e991e147dce0756fb243a7e88562f599001
igoose87 <307518@niuitmo.ru>
2024-01-19 12:12:19 +0300

    feat: added html, click, addClass and removeClass methods

--- src/core/rquery/rquery.lib.js
+++ src/core/rquery/rquery.lib.js
@@ -69,6 +69,34 @@ class RQuery {
   }
 
   /**
+   * Get or set the inner HTML of the selected element.
+   * @param {string} [htmlContent] - Optional HTML content to set. 
+   * If not provided, the current inner HTML will be returned.
+   * @returns {RQuery|string} The current RQuery instance for chaining when setting HTML content, 
+   * or the current inner HTML when getting
+   */
+  html(htmlContent){
+    if (typeof htmlContent === undefined){
+      return this.element.innerHTML
+    } else {
+      this.element.innerHTML = htmlContent
+      return this
+    }
+  }
+
+  /**
+   * Attach a click event listener to the selected element.
+   * @param {function(Event): void} callback - The event listener function
+   * to execute when the selected element is clicked. 
+   * The function will receive the event object as its argument.
+  * @returns {RQuery} The current  RQuery instance for chaining.
+   */
+  click(callback){
+    this.element.addEventListener('click', callback)
+    return this
+  }
+
+  /**
    * Set the CSS style of the selected element.
    * @param {string} property - the CSS property to set.
    * @param {string} value - The value to set for the CSS property.
@@ -80,6 +108,42 @@ class RQuery {
     }
 
     this.element.style[property] = value
+    return this
+  }
+  
+  /**
+   * Adds a class or a list of classes to the current element.
+   * @param {string | string[]} classNames - A single class name or an array 
+   * of class names to add to the element.
+   * @returns  {RQuery} The current RQuery instance for chaining.
+   */
+  addClass(classNames){
+    if(Array.isArray(classNames)){
+      for(const className of classNames){
+        this.element.classList.add(className)
+      }
+    } else {
+      this.element.classList.add(classNames)
+    }
+
+    return this
+  }
+
+  /**
+   * Removes a class or a list of classes from the current element.
+   * @param {string | string[]} classNames - A single class name or an array 
+   * of class names to remove from the element.
+   * @returns {RQuery} The current RQuery instance for chaining.
+   */
+  removeClass(classNames){
+    if(Array.isArray(classNames)){
+      for(const className of classNames){
+        this.element.classList.remove(className)
+      }
+    } else {
+      this.element.classList.remove(classNames)
+    }
+
     return this
   }
 }
