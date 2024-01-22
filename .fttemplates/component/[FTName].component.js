c0db22014f92d323e3756b2ce62196bbc275aeb1
igoose87 <307518@niuitmo.ru>
2024-01-22 12:46:01 +0300

    fix: fixed import

--- .fttemplates/component/[FTName].component.js
+++ .fttemplates/component/[FTName].component.js
@@ -1,12 +1,12 @@
 import ChildComponent from '@/core/component/child.component'
-import RenderService from '@/core/services/render.services'
+import renderService from "@/core/services/render.service";
 
 import styles from './[FTName].module.scss'
 import template from './[FTName].template.html'
 
 export class [FTName | pascalcase] extends ChildComponent {
   render() {
-    this.element = RenderService.htmlToElement(template, [], styles);
+    this.element = renderService.htmlToElement(template, [], styles);
 
     return this.element;
   }
