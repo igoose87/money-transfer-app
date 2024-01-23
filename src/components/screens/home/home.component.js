6c9b937094e8b0a3df25bc27ade115d894b7db99
igoose87 <307518@niuitmo.ru>
2024-01-23 14:12:53 +0300

    deleted not necessary code

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -2,24 +2,13 @@ import { BaseScreen } from "@/core/component/base-screen.component";
 import renderService from "@/core/services/render.service";
 import template from './home.template.html'
 import styles from './home.module.scss'
-import { Field } from "@/components/ui/field/field.component";
 export class Home extends BaseScreen {
   constructor(){
     super({ title: 'Home' })
   }
   
   render(){
-    const element  = renderService.htmlToElement(
-      template, 
-      [
-        new Field({
-          name: 'test',
-          placeholder: 'Enter email',
-          variant: 'green'
-        })
-      ], 
-      styles)
-
+    const element  = renderService.htmlToElement(template, [], styles)
 
     return element
   }
