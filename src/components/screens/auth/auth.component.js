10aaba083c09aba471e87a653ed3765820734958
igoose87 <307518@niuitmo.ru>
2024-01-23 16:46:34 +0300

    fix: fixed error when open auth page

--- src/components/screens/auth/auth.component.js
+++ src/components/screens/auth/auth.component.js
@@ -1,10 +1,25 @@
-import { BaseScreen } from "@/core/component/base-screen.component";
+import ChildComponent from '@/core/component/child.component'
+import renderService from '@/core/services/render.service'
+
+import styles from './auth.module.scss'
+import template from './auth.template.html'
+import { BaseScreen } from '@/core/component/base-screen.component';
+import { Heading } from '@/components/ui/heading/heading.component';
 
 export class Auth extends BaseScreen {
-  constructor() {
-    super({title: 'Auth'})
+  constructor(){
+    super({
+      title: 'Auth'
+    })
   }
-  render(){
-    return '<p>Auth</p>'
+  render() {
+    this.element = renderService.htmlToElement(
+      template, 
+      [
+        new Heading('Auth')
+      ], 
+      styles);
+
+    return this.element;
   }
 }
