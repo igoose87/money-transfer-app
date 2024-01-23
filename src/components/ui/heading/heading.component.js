ba12b75435cfb6e63d28d9d6e3fcdd3811513a50
igoose87 <307518@niuitmo.ru>
2024-01-23 16:44:28 +0300

    fix: fixed wrong import

--- src/components/ui/heading/heading.component.js
+++ src/components/ui/heading/heading.component.js
@@ -1,5 +1,5 @@
 import ChildComponent from '@/core/component/child.component'
-import RenderService from '@/core/services/render.services'
+import renderService from '@/core/services/render.service'
 
 import styles from './heading.module.scss'
 import template from './heading.template.html'
@@ -13,7 +13,7 @@ export class Heading extends ChildComponent {
   }
 
   render() {
-    this.element = RenderService.htmlToElement(template, [], styles);
+    this.element = renderService.htmlToElement(template, [], styles);
 
     $R(this.element).text(this.title)
 
