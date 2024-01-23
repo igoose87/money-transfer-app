968168e27aeb197b3249543a2044a068af74747c
igoose87 <307518@niuitmo.ru>
2024-01-23 14:13:16 +0300

    feat: added components

--- src/components/layout/header/header.component.js
+++ src/components/layout/header/header.component.js
@@ -4,9 +4,19 @@ import styles from './header.module.scss'
 import template from './header.template.html'
 import renderService from '@/core/services/render.service';
 
+import { Logo } from './logo/logo.component';
+import { LogoutButton } from './logout-button/logout-button.component';
+import { Search } from './search/search.component';
+import { UserItem } from '@/components/ui/user-item/user-item.component';
+
 export class Header extends ChildComponent {
   render() {
-    this.element = renderService.htmlToElement(template, [], styles)
+    this.element = renderService.htmlToElement(template, [
+      Logo, new LogoutButton(), Search, new UserItem({
+        avatarPath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE4qaVzeEg9j60I9z2eR77MY6ilKM9l1J82A&usqp=CAU',
+          name: 'igoose87'
+      })
+    ], styles)
 
     return this.element;
   }
