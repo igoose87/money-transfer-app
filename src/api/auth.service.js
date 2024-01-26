b2a7f1476207a38f3ba67acfe7f9977165f25fc7
igoose87 <307518@niuitmo.ru>
2024-01-26 16:17:54 +0300

    feat: created service

---
+++ src/api/auth.service.js
@@ -0,0 +1,27 @@
+import { redQuery } from "@/core/red-query/red-query.lib"
+import { NotificationService } from "@/core/services/notification.service"
+import { StorageService } from "@/core/services/storage.service"
+
+export class AuthService {
+  #BASE_URL = '/auth'
+
+  constructor(){
+    // store
+    this.notificationService = new NotificationService()
+  }
+
+  main(type, body){
+    return redQuery({
+      path: `${this.#BASE_URL}/${type}`,
+      body,
+      onSuccess: data => {
+        // login store
+        this.notificationService.show(
+          'success',
+          'You have successfully logged in!'
+        )
+      },
+      method: 'POST'
+    })
+  }
+}
