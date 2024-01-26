e6e29531dcb5ad2dffca96b6e12b64385b25a3a0
igoose87 <307518@niuitmo.ru>
2024-01-26 16:17:21 +0300

    feat: added storageService support

--- src/core/red-query/red-query.lib.js
+++ src/core/red-query/red-query.lib.js
@@ -1,5 +1,8 @@
 import { SERVER_URL } from '@/config/url.config'
 import { extractErrorMessage } from './extract-error-message'
+import { StorageService } from '../services/storage.service'
+import { ACCESS_TOKEN_KEY } from '@/constants/auth.constants'
+import { NotificationService } from '../services/notification.service'
 
 /**
  * RedQuery is a minimalistic library for handling API requests.
@@ -30,8 +33,7 @@ export async function redQuery({
   let data = null
   const url = `${SERVER_URL}/api${path}`
 
-  /* ACCESS_TOKEN from localStorage */
-  const accessToken = ''
+  const accessToken = new StorageService().getItem(ACCESS_TOKEN_KEY)
 
   const requestOptions = {
     method,
@@ -65,7 +67,7 @@ export async function redQuery({
         onError(errorMessage)
       }
 
-      /* Notification error */
+      new NotificationService.show('error', errorMessage)
     }
   } catch (errorData) {
     const errorMessage = extractErrorMessage(errorData)
