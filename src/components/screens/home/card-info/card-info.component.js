cae45928bc2cee2e5d2350370f18afb7b2892bce
igoose87 <307518@niuitmo.ru>
2024-01-26 16:17:36 +0300

    fix import

--- src/components/screens/home/card-info/card-info.component.js
+++ src/components/screens/home/card-info/card-info.component.js
@@ -1,12 +1,12 @@
 import ChildComponent from '@/core/component/child.component'
-import RenderService from '@/core/services/render.services'
 
 import styles from './card-info.module.scss'
 import template from './card-info.template.html'
+import renderService from '@/core/services/render.service'
 
 export class CardInfo extends ChildComponent {
   render() {
-    this.element = RenderService.htmlToElement(template, [], styles);
+    this.element = renderService.htmlToElement(template, [], styles);
 
     return this.element;
   }
