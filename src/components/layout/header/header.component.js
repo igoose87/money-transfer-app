9e26130e7baeb15d06743584adc0a6533e585c4e
igoose87 <307518@niuitmo.ru>
2024-02-02 16:37:57 +0300

    feat: added update() method

--- src/components/layout/header/header.component.js
+++ src/components/layout/header/header.component.js
@@ -8,12 +8,33 @@ import { Logo } from './logo/logo.component';
 import { LogoutButton } from './logout-button/logout-button.component';
 import { Search } from './search/search.component';
 import { UserItem } from '@/components/ui/user-item/user-item.component';
+import { Store } from '@/core/store/store';
+import { $R } from '@/core/rquery/rquery.lib';
 
 export class Header extends ChildComponent {
   constructor({router}){
     super()
+
+    this.store = Store.getInstance()
+    
+    this.store.addObserver(this)
+
     this.router = router
   }
+
+  update(){
+    this.user = this.store.state.user
+
+    const authSideElement = $R(this.element).find('#auth-side')
+    
+    if(this.user){
+      authSideElement.show()
+      this.router.navigate('/')
+    } else {
+      authSideElement.hide()
+    }
+  }
+
   render() {
     this.element = renderService.htmlToElement(
       template, 
@@ -29,6 +50,7 @@ export class Header extends ChildComponent {
         })
       ], 
     styles)
+    this.update()
 
     return this.element;
   }
