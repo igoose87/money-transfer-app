312f7ddb7e8fdc8913cb72a9afff916165e6faa4
igoose87 <307518@niuitmo.ru>
2024-02-02 16:37:10 +0300

    feat: added Store support

--- src/api/card.service.js
+++ src/api/card.service.js
@@ -1,11 +1,12 @@
 import { redQuery } from "@/core/red-query/red-query.lib"
 import { NotificationService } from "@/core/services/notification.service"
+import { Store } from '@/core/store/store'
 
 export class CardService {
   #BASE_URL = '/cards'
 
   constructor(){
-    // store
+    this.store = Store.getInstance()
     this.notificationService = new NotificationService()
   }
 
@@ -63,8 +64,8 @@ export class CardService {
       method: 'PATCH',
       body: {
         amount: +amount,
-        // fromCardNumber: this.store.user.card.number,
-        // toCardNumber
+        fromCardNumber: this.store.user.card.number,
+        toCardNumber
       },
       onSuccess: () => {
         this.notificationService.show(
