0cec10a231e7b137054c213d4ea4f5924dc431e6
igoose87 <307518@niuitmo.ru>
2024-02-05 17:51:13 +0300

    feat: created working home component

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -2,13 +2,14 @@ import { BaseScreen } from "@/core/component/base-screen.component";
 import renderService from "@/core/services/render.service";
 import template from './home.template.html'
 import styles from './home.module.scss'
+import { CardInfo } from "./card-info/card-info.component";
 export class Home extends BaseScreen {
   constructor(){
     super({ title: 'Home' })
   }
   
   render(){
-    const element  = renderService.htmlToElement(template, [], styles)
+    const element  = renderService.htmlToElement(template, [CardInfo], styles)
 
     return element
   }
