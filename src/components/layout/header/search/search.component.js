e1bf624d6d40f9a545ceb597a227560851dec1e9
igoose87 <307518@niuitmo.ru>
2024-02-05 17:52:28 +0300

    fix

--- src/components/layout/header/search/search.component.js
+++ src/components/layout/header/search/search.component.js
@@ -1,19 +1,63 @@
 import ChildComponent from '@/core/component/child.component'
 import renderService from '@/core/services/render.service'
 
+import { $R } from '@/core/rquery/rquery.lib'
+import { UserService } from '@/api/user.service'
+import { UserItem } from '@/components/ui/user-item/user-item.component'
+import { debounce } from '@/utils/debounce.util'
+
 import styles from './search.module.scss'
 import template from './search.template.html'
-import { $R } from '@/core/rquery/rquery.lib';
 
 export class Search extends ChildComponent {
+  constructor(){
+    super()
+    this.userService = new UserService()
+  }
+
+  #handleSearch = async event => {
+    const searchTerm = event.target.value                               // take value from input
+    const searchResultElement = $R(this.element).find('#search-results')
+
+    if (!searchTerm){
+      searchResultElement.html('')
+      return
+    }
+
+    await this.userService.getAll(searchTerm, users => {
+      searchResultElement.html('')
+
+      users.forEach((user, index) => {
+        const userItem = new UserItem(user, true, () => {
+          searchResultElement.html('')
+        }).render()
+
+        $R(userItem)
+          .addClass(styles.item)
+          .css('transition-delay', `${index * 0.1}s`)
+
+        searchResultElement.append(userItem)
+
+        setTimeout(() => {                            // because .item capacity 0
+          $R(userItem).addClass(styles.visible)
+        }, 50)
+      })
+    })
+  }
+
   render() {
     this.element = renderService.htmlToElement(template, [], styles)
 
-    $R(this.element).find('input').input({
+    const debouncedHandleSearch = debounce(this.#handleSearch, 300)
+
+    $R(this.element)
+    .find('input')
+    .input({
         type: 'search',
         name: 'search',
         placeholder: 'Search contacts...'
       })
+      .on('input', debouncedHandleSearch)
 
     return this.element
   }
