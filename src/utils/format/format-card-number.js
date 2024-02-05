3324ef1ad9a85da59b5b6f0a89792e9883fb4cd0
igoose87 <307518@niuitmo.ru>
2024-02-05 17:50:42 +0300

    feat: added handling card number methods

--- src/utils/format/format-card-number.js
+++ src/utils/format/format-card-number.js
@@ -7,3 +7,19 @@
 export function formatCardNumberWithDashes(cardNumber){
   return cardNumber.replace(/(\d{4})(?=\d)/g, '$1-')
 }
+
+/**
+ * Formats a credit card number into the format **** **** **** ****.
+ * 
+ * @param {string} cardNumber - The credit card number consisting of 16 digits without separators.
+ * @returns {string} - The formatted credit card number.
+ */
+
+/*
+ Удаляет все пробелы из номера карты и разбивает его на группы из 4ех цифр с помощью регулярного выражения /.{1,4}/g.
+ Затем функция объеденяет эти группы в одну строку с пробелами между ними, используя метод join()
+*/
+export function formatCardNumber(cardNumber){
+  const formattedNumber = cardNumber.replace(/\s/g, '').match(/.{1,4}/g)
+  return formattedNumber ? formattedNumber.join(' ') : ''
+}
