fdcb2471ff8076287a7e773d2f019081c28fb228
igoose87 <307518@niuitmo.ru>
2024-02-06 17:28:10 +0300

    feat: added action component support

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -3,13 +3,19 @@ import renderService from "@/core/services/render.service";
 import template from './home.template.html'
 import styles from './home.module.scss'
 import { CardInfo } from "./card-info/card-info.component";
+import { Actions } from "./actions/actions.component";
+
 export class Home extends BaseScreen {
   constructor(){
     super({ title: 'Home' })
   }
   
   render(){
-    const element  = renderService.htmlToElement(template, [CardInfo], styles)
+    const element  = renderService.htmlToElement(
+      template, 
+      [CardInfo, Actions], 
+      styles
+      )
 
     return element
   }
