f942c1701577212b7580a6cf981ca0ec0710d45f
igoose87 <307518@niuitmo.ru>
2024-02-08 15:19:25 +0300

    feat: added update balance after money operation

--- src/components/screens/home/actions/actions.component.js
+++ src/components/screens/home/actions/actions.component.js
@@ -8,6 +8,8 @@ import { NotificationService } from '@/core/services/notification.service';
 import { Field } from '@/components/ui/field/field.component';
 import { Button } from '@/components/ui/button/button.component';
 import { $R } from '@/core/rquery/rquery.lib';
+import validationService from '@/core/services/validation.service';
+import { BALANCE_UPDATED } from '@/constants/event.constants';  
 
 export class Actions extends ChildComponent {
   constructor(){
@@ -18,30 +20,67 @@ export class Actions extends ChildComponent {
     this.notificationService = new NotificationService()
   }
 
-  render() {
-    this.element = renderService.htmlToElement(template, 
-    [
-      new Field({
-        name: 'amount',
-        placeholder: 'Enter amount:',
-        type: 'number'
-      })
-    ], 
-    styles
-  )
+  /**
+   * @param {Event} event - The event object from the button click event.
+   * @param {'top-up' | 'withdrawal'} type - The type of the transaction, 
+   * either "top-up" or "withdrawal".
+   */
+  updateBalance(event, type){
+    event.preventDefault()
 
+    if (!this.store.user) {
+      this.notificationService.show('error', 'You need authorization!')
+    }
+
+    $R(event.target).text('Sending...').attr('disabled', true)
+
+    const inputElement = $R(this.element).find('input')
+    const amount = inputElement.value()
+
+    // red border around field
+    if (!amount){
+      validationService.showError($R(this.element).find('label'))
+      return
+    }
+
+    this.cardService.updateBalance(amount, type, () => {
+      inputElement.value('')
+
+      const balanceUpdatedEvent = new Event(BALANCE_UPDATED)
+      document.dispatchEvent(balanceUpdatedEvent)
+    })
+
+    $R(event.target).removeAttr('disabled').text(type)
+  }
+
+  render() {
+    this.element = renderService.htmlToElement(
+      template, 
+      [
+        new Field({
+          name: 'amount',
+          placeholder: 'Enter amount:',
+          type: 'number'
+        })
+      ], 
+      styles
+    )
+
+    //! variant doesn't work idk why
     $R(this.element)
       .find('#action-buttons')
       .append(
         new Button({
           children: 'Top-up',
-          variant: 'green'
+          variant: 'green',
+          onClick: e => this.updateBalance(e, 'top-up')
         }).render()
       )
       .append(
         new Button({
           children: 'Withdrawal',
-          variant: 'purple'
+          variant: 'purple',
+          onClick: e => this.updateBalance(e, 'withdrawal')
         }).render()
       )
 
