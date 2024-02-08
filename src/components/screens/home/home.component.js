ff025b365f1a4ff44a4dc79a36cc412395fd9154
igoose87 <307518@niuitmo.ru>
2024-02-08 17:51:02 +0300

    feat: added component support

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -4,6 +4,7 @@ import template from './home.template.html'
 import styles from './home.module.scss'
 import { CardInfo } from "./card-info/card-info.component";
 import { Actions } from "./actions/actions.component";
+import { Contacts } from "./contacts/contacts.component";
 
 export class Home extends BaseScreen {
   constructor(){
@@ -13,7 +14,7 @@ export class Home extends BaseScreen {
   render(){
     const element  = renderService.htmlToElement(
       template, 
-      [CardInfo, Actions], 
+      [CardInfo, Actions, Contacts], 
       styles
       )
 
